c0fc832427e1b5ccd79246703b166432b223a4fc
Halima <halimakathermathersha@gmail.com>
2026-06-07 18:26:02 +0530

    update

---
+++ Js/Day22/intro.js
@@ -0,0 +1,225 @@
+var computerApplication;
+console.log(computerApplication)
+var simCard
+console.log(simCard)
+var teleVision;
+console.log(teleVision)
+var iceCream;
+console.log(iceCream)
+var bannerCard
+console.log(bannerCard)
+var mobilePhone;
+console.log(mobilePhone)
+var waterBottle;
+console.log(waterBottle)
+var laptopBag;
+console.log(laptopBag)
+var bluetoothSpeaker;
+console.log(bluetoothSpeaker)
+var schoolBus;
+console.log(schoolBus)
+var coffeeCup;
+console.log(coffeeCup)
+var smartWatch;
+console.log(smartWatch)
+var cricketBat;
+console.log(cricketBat)
+var movieTicket;
+console.log(movieTicket)
+var gamingMouse;
+console.log(gamingMouse)
+var faceWash;
+console.log(faceWash)
+var diningTable;
+console.log(diningTable)
+var keyboardLight;
+console.log(keyboardLight)
+var travelBag;
+console.log(travelBag)
+
+var chocolateBox;
+console.log(chocolateBox)
+
+
+
+let redCar;
+console.log(redCar)
+
+let bluePen;
+console.log(bluePen)
+
+let greenBottle;
+console.log(greenBottle)
+
+let whiteBoard;
+console.log(whiteBoard)
+
+let blackShoes;
+console.log(blackShoes)
+
+let yellowBag;
+console.log(yellowBag)
+
+let silverWatch;
+console.log(silverWatch)
+
+let orangeJuice;
+console.log(orangeJuice)
+
+let mobileCharger;
+console.log(mobileCharger)
+
+let ceilingFan;
+console.log(ceilingFan)
+
+let studyTable;
+console.log(studyTable)
+
+let wirelessMouse;
+console.log(wirelessMouse)
+
+let cottonShirt;
+console.log(cottonShirt)
+
+let plasticChair;
+console.log(plasticChair)
+
+let officeLaptop;
+console.log(officeLaptop)
+
+let gamingKeyboard;
+console.log(gamingKeyboard)
+
+let kitchenStove;
+console.log(kitchenStove)
+
+let cameraStand;
+console.log(cameraStand)
+
+let flowerPot;
+console.log(flowerPot)
+
+let musicSpeaker;
+console.log(musicSpeaker)
+
+let sportsCycle;
+console.log(sportsCycle)
+
+let leatherWallet;
+console.log(leatherWallet)
+
+let electricScooter;
+console.log(electricScooter)
+
+let desktopMonitor;
+console.log(desktopMonitor)
+
+let airConditioner;
+console.log(airConditioner)
+
+let glassWindow;
+console.log(glassWindow)
+
+let woodenDoor;
+console.log(woodenDoor)
+
+let mountainBike;
+console.log(mountainBike)
+
+let trainTicket;
+console.log(trainTicket)
+
+let rainCoat;
+console.log(rainCoat)
+
+
+
+const appleFruit;
+console.log(appleFruit)
+
+const mangoJuice;
+console.log(mangoJuice)
+
+const cityName;
+console.log(cityName)
+
+const countryName;
+console.log(countryName)
+
+const studentName;
+console.log(studentName)
+
+const collegeName;
+console.log(collegeName)
+
+const laptopBrand;
+console.log(laptopBrand)
+
+const mobileBrand;
+console.log(mobileBrand)
+
+const browserName;
+console.log(browserName)
+
+const subjectName;
+console.log(subjectName)
+
+const movieName;
+console.log(movieName)
+
+const gameName;
+console.log(gameName)
+
+const petName;
+console.log(petName)
+
+const teacherName;
+console.log(teacherName)
+
+const foodName;
+console.log(foodName)
+
+const drinkName;
+console.log(drinkName)
+
+const bikeName;
+console.log(bikeName)
+
+const carName;
+console.log(carName)
+
+const languageName;
+console.log(languageName)
+
+const appName;
+console.log(appName)
+
+const shoeBrand;
+console.log(shoeBrand)
+
+const festivalName;
+console.log(festivalName)
+
+const actorName;
+console.log(actorName)
+
+const actressName;
+console.log(actressName)
+
+const hospitalName;
+console.log(hospitalName)
+
+const shopName;
+console.log(shopName)
+
+const flowerName;
+console.log(flowerName)
+
+const animalName;
+console.log(animalName)
+
+const colorName;
+console.log(colorName)
+
+const seasonName;
+console.log(seasonName)
